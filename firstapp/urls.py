d09a19efa3bf9f9a09b365e280a0beff4171466e
Safiqul Islam <safiqulislam.green@gmail.com>
2023-03-08 23:43:30 +0600

    future relocation first push

--- firstapp/urls.py
+++ firstapp/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index, name='homePage'),
+    path('about/', views.about, name='aboutPage'),
+    path('portfolio/', views.portfolio, name='portfolioPage')
 ]
--- firstapp/views.py
+++ firstapp/views.py
@@ -3,8 +3,12 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'home.html')
+    return render(request, 'frontend/home.html')
 
 
+def about(request):
+    return render(request, 'frontend/about.html')
 
 
+def portfolio(request):
+    return render(request, 'frontend/portfolio.html')
